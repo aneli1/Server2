1579b76ca5b6425c04f7ffd122b2de51649b5ddf
aneli1 <148299361+aneli1@users.noreply.github.com>
2024-02-23 02:23:58 -0600

    Add files via upload

---
+++ build/keys.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//exportamos los datos por default de la base de datos,
+// o los datos que le hayamos colocado
+exports.default = {
+    database: {
+        host: 'ls-3e0cc6d0d027fdd2cfbc09a148a4ec0c40bde812.cfsoci82q3h4.us-east-2.rds.amazonaws.com',
+        user: 'dbmasteruser',
+        password: 'aguacate',
+        database: 'consultorio'
+    }
+};
